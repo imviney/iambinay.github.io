6f2aa3bbb8b77a28af4e6555c820039f948d2fd0
Binay <30464789+GwaraDon@users.noreply.github.com>
2019-07-22 16:42:07 +0545

    Add files via upload

---
+++ precache-manifest.1799e88f14c415e9d890344f06a1e671.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d74a36702354c514ce",
+    "url": "/js/chunk-vendors.1ec9e2a8.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "85f1290bae5d25df24dd",
+    "url": "/js/app.e4269e28.js"
+  },
+  {
+    "revision": "d7b1be226b210ab1cb42",
+    "url": "/js/about.7ec6cb2d.js"
+  },
+  {
+    "revision": "3783e42ccca746f6053294678642e316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05eb1e70cd78bf016b7f1f42b446cf23",
+    "url": "/img/bg.05eb1e70.jpg"
+  },
+  {
+    "revision": "4e7309c70bff8a021147813adbb34150",
+    "url": "/img/about.4e7309c7.png"
+  },
+  {
+    "revision": "b032715cc5ad4e14f3668dbddb0e6217",
+    "url": "/img/Binay1.b032715c.png"
+  },
+  {
+    "revision": "72d74a36702354c514ce",
+    "url": "/css/chunk-vendors.52082ea8.css"
+  },
+  {
+    "revision": "85f1290bae5d25df24dd",
+    "url": "/css/app.316e483a.css"
+  },
+  {
+    "revision": "d7b1be226b210ab1cb42",
+    "url": "/css/about.53d2d01a.css"
+  }
+];
